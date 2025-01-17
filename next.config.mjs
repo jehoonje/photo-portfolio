e94c37005ea1fc33c6a1cdc9d0b34a6483948412
Jehoon <limjhoon8@gmail.com>
2025-01-17 14:00:34 +0900

    🚧 In progress for gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'export',
+  basePath: '/photo-portfolio',
+  assetPrefix: '/photo-portfolio',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
